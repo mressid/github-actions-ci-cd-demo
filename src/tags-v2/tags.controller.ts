1a1bbc5d1abaf7f2afbcbbd0dfcb9f77dd513622
vitamin-0xff <essid101010@gmail.com>
2026-06-18 13:06:11 +0100

    add tags v2 endpoints

---
+++ src/tags-v2/tags.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { TagsService } from './tags.service';
+
+@ApiTags('tags-v2')
+@Controller('tags-v2')
+export class TagsController {
+  constructor(private readonly tags: TagsService) {}
+
+  @Get()
+  findAll() {
+    return this.tags.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.tags.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body: { name: string }) {
+    return this.tags.create(body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    this.tags.remove(id);
+    return { deleted: true };
+  }
+}
